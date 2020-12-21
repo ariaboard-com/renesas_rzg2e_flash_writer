d3b30a9220a80e3fcdeaad53eb1a4fc055a70465
Kyosuke Nekomura <supercatexpert@gmail.com>
2020-12-21 20:22:12 +0900

    Added build scripts.

---
+++ build-spi.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export CROSS_COMPILE=$(pwd)/../gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+make -f makefile.linaro BOARD=EK874 EMMC=DISABLE SERIAL_FLASH=ENABLE USB=ENABLE
