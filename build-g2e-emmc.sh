9b90c1963f19721c1409151bba9f394193a5e3da
Kyosuke Nekomura <supercatexpert@gmail.com>
2021-03-15 18:01:04 +0900

    Don't compile USB support.

--- build-g2e-emmc.sh
+++ build-g2e-emmc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export CROSS_COMPILE=$(pwd)/../gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-make -f makefile.linaro BOARD=EK874 EMMC=ENABLE SERIAL_FLASH=DISABLE USB=ENABLE
+make -f makefile.linaro BOARD=EK874 EMMC=ENABLE SERIAL_FLASH=DISABLE
